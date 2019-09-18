373115808679fd4f327ea055313ac7cb6f4fa714
piplongrun <piplongrun@users.noreply.github.com>
2019-09-19 00:24:42 +0200

    Copy initial config file into place when one doesn't exist yet
    By writing the config file to the mounted directory at this stage, the config file becomes visible on the host system.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,6 +10,11 @@ if [ -n "$UID" ]; then
     usermod -o -u "$UID" radicale
 fi
 
+# If no config file exists copy the default one to the config folder
+if [[ ! -e /config/config ]]; then
+    cp /opt/radicale/config /config
+fi
+
 # Re-set permission to the `radicale` user if current user is root
 # This avoids permission denied if the data volume is mounted by root
 if [ "$1" = 'radicale' ] && [ "$(id -u)" = '0' ]; then
